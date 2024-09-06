a87bc0a1b5322228222ea22c3bf4f73c855de701
Atarian-ByVoid <cgomespimentel666@gmail.com>
2024-09-06 14:15:54 -0300

    style

--- src/main/java/com/hera/HeraForecastApplication.java
+++ src/main/java/com/hera/HeraForecastApplication.java
@@ -1,4 +1,4 @@
-package com.hera.hera_forecast;
+package com.hera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class HeraForecastApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeraForecastApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HeraForecastApplication.class, args);
+    }
 
 }
